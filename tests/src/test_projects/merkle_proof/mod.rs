656fa9f699e31d2c1aa30572b62765de2aa837ef
bitzoic <cameron.carstens@fuel.sh>
2022-07-21 13:54:37 -0400

    Update SDK v0.18

--- tests/src/test_projects/merkle_proof/mod.rs
+++ tests/src/test_projects/merkle_proof/mod.rs
@@ -1,12 +1,14 @@
-use fuels::prelude::*;
+use fuels::{prelude::*, tx::ContractId};
 
 abigen!(
     TestMerkleProofLib,
     "test_projects/merkle_proof/out/debug/merkle_proof-abi.json"
 );
 
-async fn test_merkle_proof_instance() -> TestMerkleProofLib {
+async fn test_merkle_proof_instance() -> (TestMerkleProofLib, ContractId) {
+    // Launch a local network and deploy the contract
     let wallet = launch_provider_and_get_wallet().await;
+
     let id = Contract::deploy(
         "test_projects/merkle_proof/out/debug/merkle_proof.bin",
         &wallet,
@@ -16,30 +18,32 @@ async fn test_merkle_proof_instance() -> TestMerkleProofLib {
     .await
     .unwrap();
 
-    TestMerkleProofLib::new(id.to_string(), wallet)
+    let instance = MyContractBuilder::new(id.to_string(), wallet).build();
+
+    (instance, id)
 }
 
-mod merkle_proof {
+// mod merkle_proof {
 
-    use super::*;
+//     use super::*;
 
-    mod success {
+//     mod success {
 
-        use super::*;
+//         use super::*;
 
-        #[tokio::test]
-        async fn verifies_merkle_proof() {
-            let _instance = test_merkle_proof_instance().await;
+//         #[tokio::test]
+//         async fn verifies_merkle_proof() {
+//             let _instance = test_merkle_proof_instance().await;
 
-            // Test here once https://github.com/FuelLabs/fuels-rs/issues/353 is closed
-        }
+//             // Test here once https://github.com/FuelLabs/fuels-rs/issues/353 is closed
+//         }
 
-    }
+//     }
 
-    mod reverts {
+//     mod reverts {
 
-        use super::*;
+//         use super::*;
 
-    }
+//     }
 
-}
+// }
