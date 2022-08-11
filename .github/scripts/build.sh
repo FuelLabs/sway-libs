4345d17ee80be76b677894784d2cb4b1f5d1d611
bitzoic <cameron.carstens@fuel.sh>
2022-08-11 15:02:07 -0400

    Remove lock from build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# Pass `--locked` when running this script in CI to ensure both cargo and forc
-# lock files are up to date.
-locked="$1"
-
 # Grab the absolute path to this script.
 test_dirs="tests/src/test_projects/*"
 
 for test_dir in $test_dirs; do
   if [ -f "${test_dir}/Forc.toml" ]; then
     echo "Building test $test_dir..."
-    forc build $locked -o temp -p "${test_dir}" && echo ✔
+    forc build -o temp -p "${test_dir}" && echo ✔
     if ! [[ -f temp ]]; then
       echo  "❌  Failed to build $test_dir"
       exit 1
