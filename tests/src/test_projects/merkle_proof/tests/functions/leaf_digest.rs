d58d656a9c24f168e2032fb49a12ace1875404ca
bitzoic <cameron.carstens@fuel.sh>
2022-08-30 12:45:43 -0400

    Update test based on suggestions

--- tests/src/test_projects/merkle_proof/tests/functions/leaf_digest.rs
+++ tests/src/test_projects/merkle_proof/tests/functions/leaf_digest.rs
@@ -3,27 +3,22 @@ use crate::merkle_proof::tests::utils::{
     test_helpers::{build_tree, merkle_proof_instance},
 };
 
-mod leaf_digest {
+mod success {
 
     use super::*;
 
-    mod success {
+    #[ignore]
+    #[tokio::test]
+    async fn computes_leaf() {
+        let instance = merkle_proof_instance().await;
 
-        use super::*;
+        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let key = 0;
+        let (_tree, _root, leaf, _proof) = build_tree(leaves, key).await;
 
-        #[ignore]
-        #[tokio::test]
-        async fn computes_leaf() {
-            let instance = merkle_proof_instance().await;
-
-            let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
-            let key = 0;
-            let (_tree, _root, leaf, _proof) = build_tree(leaves, key).await;
-
-            assert_eq!(
-                leaf_digest(&instance, "A".as_bytes().try_into().unwrap()).await,
-                leaf
-            );
-        }
+        assert_eq!(
+            leaf_digest(&instance, "A".as_bytes().try_into().unwrap()).await,
+            leaf
+        );
     }
 }
--- tests/src/test_projects/merkle_proof/tests/functions/verify_proof.rs
+++ tests/src/test_projects/merkle_proof/tests/functions/verify_proof.rs
@@ -3,54 +3,49 @@ use crate::merkle_proof::tests::utils::{
     test_helpers::{build_tree, merkle_proof_instance},
 };
 
-mod verify_merkle_proof {
+mod success {
 
     use super::*;
 
-    mod success {
+    #[ignore]
+    #[tokio::test]
+    async fn fails_merkle_proof_verification() {
+        let instance = merkle_proof_instance().await;
 
-        use super::*;
+        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let num_leaves = 3;
+        let key = 0;
 
-        #[ignore]
-        #[tokio::test]
-        async fn fails_merkle_proof_verification() {
-            let instance = merkle_proof_instance().await;
+        let (_tree, root, leaf, proof) = build_tree(leaves, key).await;
 
-            let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
-            let num_leaves = 3;
-            let key = 0;
+        assert_eq!(
+            verify_proof(
+                &instance,
+                key,
+                leaf,
+                root,
+                num_leaves,
+                [proof[1], proof[0]].to_vec()
+            )
+            .await,
+            false
+        );
+    }
 
-            let (_tree, root, leaf, proof) = build_tree(leaves, key).await;
+    #[ignore]
+    #[tokio::test]
+    async fn verifies_merkle_proof() {
+        let instance = merkle_proof_instance().await;
 
-            assert_eq!(
-                verify_proof(
-                    &instance,
-                    key,
-                    leaf,
-                    root,
-                    num_leaves,
-                    [proof[1], proof[0]].to_vec()
-                )
-                .await,
-                false
-            );
-        }
+        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let num_leaves = 3;
+        let key = 0;
 
-        #[ignore]
-        #[tokio::test]
-        async fn verifies_merkle_proof() {
-            let instance = merkle_proof_instance().await;
+        let (_tree, root, leaf, proof) = build_tree(leaves, key).await;
 
-            let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
-            let num_leaves = 3;
-            let key = 0;
-
-            let (_tree, root, leaf, proof) = build_tree(leaves, key).await;
-
-            assert_eq!(
-                verify_proof(&instance, key, leaf, root, num_leaves, proof).await,
-                true
-            );
-        }
+        assert_eq!(
+            verify_proof(&instance, key, leaf, root, num_leaves, proof).await,
+            true
+        );
     }
 }
