afe18bffae002e045b5797fed0f6dc4c04618b56
bitzoic <cameron.carstens@fuel.sh>
2022-08-30 18:04:21 -0400

    Add approved tests

--- tests/src/test_projects/nft/tests/functions/approve.rs
+++ tests/src/test_projects/nft/tests/functions/approve.rs
@@ -10,7 +10,7 @@ mod success {
     use super::*;
 
     #[tokio::test]
-    async fn gets_approval() {
+    async fn approves() {
         let (deploy_wallet, owner1, owner2) = setup().await;
 
         // constructor(false, &deploy_wallet.contract, &Option::None(), 1).await;
@@ -28,48 +28,35 @@ mod success {
     }
 
     #[tokio::test]
-    async fn gets_approval_multiple() {
+    async fn approves_mutliple() {
         let (deploy_wallet, owner1, owner2) = setup().await;
 
         // constructor(false, &deploy_wallet.contract, &Option::None(), 1).await;
         let admin = Identity::Address(owner1.wallet.address().into());
-        constructor(true, &deploy_wallet.contract, &admin, 3).await;
+        constructor(true, &deploy_wallet.contract, &admin, 4).await;
 
         let minter = Identity::Address(owner1.wallet.address().into());
-        mint(3, &owner1.contract, &minter).await;
+        mint(4, &owner1.contract, &minter).await;
 
         // let approved_identity = Option::Some(Identity::Address(owner2.wallet.address().into()));
         let approved_identity = Identity::Address(owner2.wallet.address().into());
         approve(&approved_identity, &owner1.contract, 0).await;
-        approve(&approved_identity, &owner1.contract, 1).await;
-        approve(&approved_identity, &owner1.contract, 2).await;
-
         assert_eq!(approved(&owner1.contract, 0).await, approved_identity);
+
+        // let approved_identity = Option::Some(Identity::Address(owner2.wallet.address().into()));
+        let approved_identity = Identity::Address(owner2.wallet.address().into());
+        approve(&approved_identity, &owner1.contract, 1).await;
         assert_eq!(approved(&owner1.contract, 1).await, approved_identity);
+
+        // let approved_identity = Option::Some(Identity::Address(owner2.wallet.address().into()));
+        let approved_identity = Identity::Address(owner2.wallet.address().into());
+        approve(&approved_identity, &owner1.contract, 2).await;
         assert_eq!(approved(&owner1.contract, 2).await, approved_identity);
-    }
 
-    #[tokio::test]
-    #[ignore]
-    async fn gets_approval_for_none() {
-        let (_deploy_wallet, owner1, _owner2) = setup().await;
-
-        // constructor(false, &deploy_wallet.contract, &Option::None(), 1).await;
-
-        let minter = Identity::Address(owner1.wallet.address().into());
-        mint(1, &owner1.contract, &minter).await;
-
-        // assert_eq!(approved(&owner1.contract, 0).await, Option::None());
-    }
-
-    #[tokio::test]
-    #[ignore]
-    async fn gets_approval_for_non_existing_token() {
-        let (_deploy_wallet, _owner1, _owner2) = setup().await;
-
-        // constructor(false, &deploy_wallet.contract, &Option::None(), 1).await;
-
-        // assert_eq!(approved(&owner1.contract, 0).await, Option::None());
+        // let approved_identity = Option::Some(Identity::Address(owner2.wallet.address().into()));
+        let approved_identity = Identity::Address(owner2.wallet.address().into());
+        approve(&approved_identity, &owner1.contract, 3).await;
+        assert_eq!(approved(&owner1.contract, 3).await, approved_identity);
     }
 }
 
@@ -79,26 +66,42 @@ mod reverts {
 
     #[tokio::test]
     #[should_panic(expected = "Revert(42)")]
-    async fn gets_approval_for_none() {
-        let (deploy_wallet, owner1, _owner2) = setup().await;
+    async fn panics_when_token_owner_does_not_exist() {
+        let (_deploy_wallet, owner1, owner2) = setup().await;
 
+        // let approved_identity = Option::Some(Identity::Address(owner2.wallet.address().into()));
+        let approved_identity = Identity::Address(owner2.wallet.address().into());
+        approve(&approved_identity, &owner1.contract, 0).await;
+    }
+
+    #[tokio::test]
+    #[should_panic(expected = "Revert(42)")]
+    async fn panics_when_token_does_not_map_to_existing_token() {
+        let (deploy_wallet, owner1, owner2) = setup().await;
+
+        // constructor(false, &deploy_wallet.contract, &Option::None(), 1).await;
+        let admin = Identity::Address(owner1.wallet.address().into());
+        constructor(true, &deploy_wallet.contract, &admin, 1).await;
+
+        // let approved_identity = Option::Some(Identity::Address(owner2.wallet.address().into()));
+        let approved_identity = Identity::Address(owner2.wallet.address().into());
+        approve(&approved_identity, &owner1.contract, 0).await;
+    }
+
+    #[tokio::test]
+    #[should_panic(expected = "Revert(42)")]
+    async fn panics_when_sender_is_not_owner() {
+        let (deploy_wallet, owner1, owner2) = setup().await;
+
+        // constructor(false, &deploy_wallet.contract, &Option::None(), 1).await;'
         let admin = Identity::Address(owner1.wallet.address().into());
         constructor(true, &deploy_wallet.contract, &admin, 1).await;
 
         let minter = Identity::Address(owner1.wallet.address().into());
         mint(1, &owner1.contract, &minter).await;
 
-        approved(&owner1.contract, 0).await;
-    }
-
-    #[tokio::test]
-    #[should_panic(expected = "Revert(42)")]
-    async fn gets_approval_for_non_existing_token() {
-        let (deploy_wallet, owner1, _owner2) = setup().await;
-
-        let admin = Identity::Address(owner1.wallet.address().into());
-        constructor(true, &deploy_wallet.contract, &admin, 1).await;
-
-        approved(&owner1.contract, 0).await;
+        // let approved_identity = Option::Some(Identity::Address(owner2.wallet.address().into()));
+        let approved_identity = Identity::Address(owner2.wallet.address().into());
+        approve(&approved_identity, &owner2.contract, 0).await;
     }
 }
--- tests/src/test_projects/nft/tests/functions/mod.rs
+++ tests/src/test_projects/nft/tests/functions/mod.rs
@@ -1,2 +1,3 @@
 mod admin;
 mod approve;
+mod approved;
