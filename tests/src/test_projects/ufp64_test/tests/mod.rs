f958c34729e98a7c5825fcebc3cd0e2a66f9dd1e
rostyslavtyshko <106978277+rostyslavtyshko@users.noreply.github.com>
2022-12-09 17:35:39 +0100

    UFP64 (#31)
    ## Type of change
    
    <!--Delete points that do not apply-->
    
    - New feature
    
    ## Changes
    
    The following changes have been made:
    
    Addition of 64-bit fixed-point typs - `UFP64`
    
    ## Notes
    
    - Note 1
    
    ## Related Issues
    
    <!--Delete everything after the "#" symbol and replace it with a number.
    No spaces between hash and number-->
    
    Closes  #37
    
    Co-authored-by: tyshkor <tyshko1@gmail.com>
    Co-authored-by: Cameron Carstens <54727135+bitzoic@users.noreply.github.com>
    Co-authored-by: Simon <46566889+simonr0204@users.noreply.github.com>

---
+++ tests/src/test_projects/ufp64_test/tests/mod.rs
@@ -0,0 +1,13 @@
+use fuels::prelude::{script::run_compiled_script, TxParameters};
+
+mod success {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn runs_ufp64_test_script() {
+        let path_to_bin = "../out/debug/ufp64_test.bin";
+
+        let _result = run_compiled_script(path_to_bin, TxParameters::default(), None).await;
+    }
+}
