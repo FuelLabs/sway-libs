07eee0a8b8b7c7eaad5c251de28b5cb5e1df5e82
Cameron Carstens <54727135+bitzoic@users.noreply.github.com>
2023-01-11 10:32:39 -0500

    Check rust formatting in the ci  (#74)
    ## Type of change
    
    <!--Delete points that do not apply-->
    
    - Improvement (refactoring, restructuring repository, cleaning tech
    debt, ...)
    
    ## Changes
    
    The following changes have been made:
    
    - Formatted tests
    - Added rust test format check to CI
    
    ## Related Issues
    
    <!--Delete everything after the "#" symbol and replace it with a number.
    No spaces between hash and number-->
    
    Closes #71
    
    Co-authored-by: bitzoic <cameron.carstens@fuel.sh>

--- tests/src/unsigned_numbers/mod.rs
+++ tests/src/unsigned_numbers/mod.rs
@@ -1,8 +1,8 @@
+mod ufp128_div_test;
+mod ufp128_test;
 mod ufp64_div_test;
 mod ufp64_exp_test;
 mod ufp64_mul_test;
 mod ufp64_pow_test;
 mod ufp64_root_test;
 mod ufp64_test;
-mod ufp128_div_test;
-mod ufp128_test;
