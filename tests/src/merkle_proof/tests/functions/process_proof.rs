13d753f8cf3b3c3949d3f54ab980efd9e49f8575
Cameron Carstens <54727135+bitzoic@users.noreply.github.com>
2023-01-27 21:55:59 +0100

    Update Merkle Proof Library to use Bytes type (#81)
    ## Type of change
    
    <!--Delete points that do not apply-->
    
    - Improvement (refactoring, restructuring repository, cleaning tech
    debt, ...)
    
    ## Changes
    
    The following changes have been made:
    
    - `leaf_digest()` and `node_digest()` now use the `Bytes` type to
    tightly pack bytes
    - Ignored tests have now been readded
    - Some tests have been updated to use a greater number of leaves with a
    total tree depth of up to 8
    - Documentation has been updated that the library now works with the
    Fuel-Merkle repo
    
    ## Notes
    
    - The `Bytes` type does not have a `From<T>` implemented to natively
    convert a `b256`. This should be updated when
    https://github.com/FuelLabs/sway/issues/3810 is resolved
    - The `Bytes` type does not have a way to natively hash the tightly
    packed bytes. This should be updated when
    https://github.com/FuelLabs/sway/issues/3809 is resolved.
    - This should unblock the Chess and Battleship games to use Merkle proof
    verification
    
    ## Related Issues
    
    <!--Delete everything after the "#" symbol and replace it with a number.
    No spaces between hash and number-->
    
    Closes #80
    
    ---------
    
    Co-authored-by: bitzoic <cameron.carstens@fuel.sh>

--- tests/src/merkle_proof/tests/functions/process_proof.rs
+++ tests/src/merkle_proof/tests/functions/process_proof.rs
@@ -9,53 +9,71 @@ mod success {
 
     use super::*;
 
-    #[ignore]
     #[tokio::test]
     async fn fails_to_process_merkle_proof() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
-        let num_leaves = 3;
+        let depth = 8;
+        let leaves = leaves_with_depth(depth).await;
         let key = 0;
 
-        let (_tree, root, leaf, proof) = build_tree(leaves, key).await;
+        let (_tree, root, leaf, proof) = build_tree(leaves.clone(), key).await;
 
         assert_ne!(
-            process_proof(
-                &instance,
-                key,
-                leaf,
-                num_leaves,
-                [proof[1], proof[0]].to_vec()
-            )
-            .await,
+            process_proof(&instance, key + 1, leaf, leaves.len() as u64, proof).await,
             root
         );
     }
 
-    #[ignore]
     #[tokio::test]
     async fn processes_merkle_proof() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
-        let num_leaves = 3;
+        let depth = 8;
+        let leaves = leaves_with_depth(depth).await;
         let key = 0;
 
-        let (_tree, root, leaf, proof) = build_tree(leaves, key).await;
+        let (_tree, root, leaf, proof) = build_tree(leaves.clone(), key).await;
 
         assert_eq!(
-            process_proof(&instance, key, leaf, num_leaves, proof).await,
+            process_proof(&instance, key, leaf, leaves.len() as u64, proof).await,
             root
         );
     }
 
-    // NOTE: This test does not use the Fuel-Merkle library but replicates it's functionality
-    // without the use of a `u8`. Due to a `u8` being padded to a full word in Sway, the Fuel-Merkle
-    // repository sha-256 hashes and the Sway sha-256 hashes do not produce the same result. This
-    // test uses a `u64` instead for node concatentation and passes as expected. Once this is resolved
-    // this test will be modified to use a `u8` again.
-    // The issue can be tracked here: https://github.com/FuelLabs/sway/issues/2594
+    #[tokio::test]
+    async fn processes_merkle_proof_key_is_num_leaves() {
+        let instance = merkle_proof_instance().await;
+
+        let depth = 8;
+        let leaves = leaves_with_depth(depth).await;
+        let key = (leaves.len() - 1) as u64;
+
+        let (_tree, root, leaf, proof) = build_tree(leaves.clone(), key).await;
+
+        assert_eq!(
+            process_proof(&instance, key, leaf, leaves.len() as u64, proof).await,
+            root
+        );
+    }
+
+    #[tokio::test]
+    async fn processes_merkle_proof_key_is_half_num_leaves() {
+        let instance = merkle_proof_instance().await;
+
+        let depth = 8;
+        let leaves = leaves_with_depth(depth).await;
+        let key = (leaves.len() / 2) as u64;
+
+        let (_tree, root, leaf, proof) = build_tree(leaves.clone(), key).await;
+
+        assert_eq!(
+            process_proof(&instance, key, leaf, leaves.len() as u64, proof).await,
+            root
+        );
+    }
+
+    // This test does not use the Fuel-Merkle library but replicates it's functionality
     #[tokio::test]
     async fn processes_merkle_proof_manual() {
         let instance = merkle_proof_instance().await;
@@ -67,7 +85,6 @@ mod success {
         let (leaf_hash, proof, root_hash) =
             build_tree_manual(leaves.clone(), depth.try_into().unwrap(), key).await;
 
-        // This passes due to use of u64 for node concatenation
         assert_eq!(
             process_proof(
                 &instance,
@@ -86,13 +103,15 @@ mod revert {
 
     use super::*;
 
-    #[ignore]
     #[tokio::test]
     #[should_panic(expected = "InvalidProofLength")]
     async fn when_invalid_proof_length_given() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let mut leaves: Vec<[u8; 1]> = Vec::new();
+        leaves.push("A".as_bytes().try_into().unwrap());
+        leaves.push("B".as_bytes().try_into().unwrap());
+        leaves.push("C".as_bytes().try_into().unwrap());
         let num_leaves = 3;
         let key = 0;
 
@@ -109,7 +128,10 @@ mod revert {
     async fn when_invalid_num_leaves_given() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let mut leaves: Vec<[u8; 1]> = Vec::new();
+        leaves.push("A".as_bytes().try_into().unwrap());
+        leaves.push("B".as_bytes().try_into().unwrap());
+        leaves.push("C".as_bytes().try_into().unwrap());
         let num_leaves = 1;
         let key = 0;
 
@@ -126,7 +148,10 @@ mod revert {
     async fn when_key_greater_or_equal_to_num_leaves() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let mut leaves: Vec<[u8; 1]> = Vec::new();
+        leaves.push("A".as_bytes().try_into().unwrap());
+        leaves.push("B".as_bytes().try_into().unwrap());
+        leaves.push("C".as_bytes().try_into().unwrap());
         let num_leaves = 3;
         let mut key = 0;
 
@@ -141,7 +166,10 @@ mod revert {
     async fn when_key_equal_num_leaves_length() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let mut leaves: Vec<[u8; 1]> = Vec::new();
+        leaves.push("A".as_bytes().try_into().unwrap());
+        leaves.push("B".as_bytes().try_into().unwrap());
+        leaves.push("C".as_bytes().try_into().unwrap());
         let num_leaves = 4;
         let key = 0;
 
@@ -155,7 +183,10 @@ mod revert {
     async fn when_key_greater_than_num_leaves_length() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let mut leaves: Vec<[u8; 1]> = Vec::new();
+        leaves.push("A".as_bytes().try_into().unwrap());
+        leaves.push("B".as_bytes().try_into().unwrap());
+        leaves.push("C".as_bytes().try_into().unwrap());
         let num_leaves = 4;
         let mut key = 0;
 
@@ -170,7 +201,10 @@ mod revert {
     async fn when_num_leaves_zero() {
         let instance = merkle_proof_instance().await;
 
-        let leaves = ["A".as_bytes(), "B".as_bytes(), "C".as_bytes()].to_vec();
+        let mut leaves: Vec<[u8; 1]> = Vec::new();
+        leaves.push("A".as_bytes().try_into().unwrap());
+        leaves.push("B".as_bytes().try_into().unwrap());
+        leaves.push("C".as_bytes().try_into().unwrap());
         let num_leaves = 0;
         let key = 0;
 
