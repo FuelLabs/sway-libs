3c2a68bfd884e310b8f12bd73d2d1b042708ad70
rostyslavtyshko <106978277+rostyslavtyshko@users.noreply.github.com>
2023-04-15 14:44:14 +0200

    Proper test directory naming #112  (#131)
    ## Type of change
    
    - Improvement (refactoring, restructuring repository, cleaning tech
    debt, ...)
    
    
    ## Changes
    
    The following changes have been made:
    
    Renaming on `unsigned_numbers` directory to `fixed_point`
    
    
    Closes #112

---
+++ tests/src/fixed_point/ufp32_pow_test/tests/mod.rs
@@ -0,0 +1,21 @@
+use fuels::prelude::{abigen, launch_provider_and_get_wallet};
+
+abigen!(Script(
+    name = "TestUfp32Pow",
+    abi = "src/fixed_point/ufp32_pow_test/out/debug/ufp32_pow_test-abi.json"
+),);
+
+mod success {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn runs_ufp32_pow_test_script() {
+        let path_to_bin = "src/fixed_point/ufp32_pow_test/out/debug/ufp32_pow_test.bin";
+        let wallet = launch_provider_and_get_wallet().await;
+
+        let instance = TestUfp32Pow::new(wallet, path_to_bin);
+
+        let _result = instance.main().call().await;
+    }
+}
