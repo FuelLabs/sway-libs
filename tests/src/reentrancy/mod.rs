cbbde8ca046895cf0251a94240c1485ba704b191
Nick Furfaro <nfurfaro33@gmail.com>
2023-04-28 09:53:48 -0600

    Add test for cross-contract reentrancy (#147)
    ## Type of change
    
    <!--Delete points that do not apply-->
    
    - Improvement (refactoring, restructuring repository, cleaning tech
    debt, ...)
    
    
    ## Changes
    
    The following changes have been made:
    
    - added a new test case
    - added supporting new contract & abi
    - added new functions on existing contracts & abis
    
    ## Notes
    
    The test case added performs the following steps:
    
    1. The `Attack` contract's `launch_thwarted_attack_3()` function is
    triggered by a contract call from the SDK.
    2. The `Attack` contract calls into the `Target` contract's
    `cross_contract_reentrancy_denied()` function.
    3. The `Target` contract calls back to the `Attack` contract's
    `evil_callback_4()` function.
    4. The `Attack` contract calls into the `Attack Helper` contract's
    `attempt_cross_contract_reentrancy()` function.
    5. (attack vector) The `Attack Helper` contract attempts to re-enter by
    calling into the `Target` contract's
    `cross_contract_reentrancy_denied()` function(which hasn't finished
    execution of the initial call from the `Attack` contract).
    6. The attack is thwarted by the `reentrancy_guard()` function in place.

--- tests/src/reentrancy/mod.rs
+++ tests/src/reentrancy/mod.rs
@@ -8,11 +8,14 @@ use fuels::{
 
 abigen!(
     Contract(name="AttackerContract", abi="src/reentrancy/reentrancy_attacker_contract/out/debug/reentrancy_attacker_contract-abi.json"),
-    Contract(name="TargetContract", abi="src/reentrancy/reentrancy_target_contract/out/debug/reentrancy_target_contract-abi.json")
+    Contract(name="TargetContract", abi="src/reentrancy/reentrancy_target_contract/out/debug/reentrancy_target_contract-abi.json"),
+    Contract(name="AttackHelperContract", abi="src/reentrancy/reentrancy_attack_helper_contract/out/debug/reentrancy_attack_helper_contract-abi.json"),
 );
 
 const REENTRANCY_ATTACKER_BIN: &str =
     "src/reentrancy/reentrancy_attacker_contract/out/debug/reentrancy_attacker_contract.bin";
+const REENTRANCY_ATTACK_HELPER_BIN: &str =
+    "src/reentrancy/reentrancy_attack_helper_contract/out/debug/reentrancy_attack_helper_contract.bin";
 const REENTRANCY_ATTACKER_STORAGE: &str = "src/reentrancy/reentrancy_attacker_contract/out/debug/reentrancy_attacker_contract-storage_slots.json";
 
 const REENTRANCY_TARGET_BIN: &str =
@@ -51,6 +54,20 @@ pub async fn get_target_instance(
     .unwrap();
 
     let instance = TargetContract::new(id.clone(), wallet);
+
+    (instance, id.into())
+}
+
+pub async fn get_attack_helper_id(
+    wallet: WalletUnlocked,
+) -> (AttackHelperContract<WalletUnlocked>, ContractId) {
+    let id = Contract::load_from(REENTRANCY_ATTACK_HELPER_BIN, LoadConfiguration::default())
+        .unwrap()
+        .deploy(&wallet, TxParameters::default())
+        .await
+        .unwrap();
+
+    let instance = AttackHelperContract::new(id.clone(), wallet);
 
     (instance, id.into())
 }
@@ -121,6 +138,23 @@ mod revert {
             .methods()
             .launch_thwarted_attack_2(target_id)
             .set_contracts(&[&instance])
+            .call()
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    #[should_panic(expected = "NonReentrant")]
+    async fn can_block_cross_contract_reentrancy() {
+        let wallet = launch_provider_and_get_wallet().await;
+        let (attacker_instance, _) = get_attacker_instance(wallet.clone()).await;
+        let (helper_instance, helper_id) = get_attack_helper_id(wallet.clone()).await;
+        let (target_instance, target_id) = get_target_instance(wallet).await;
+
+        attacker_instance
+            .methods()
+            .launch_thwarted_attack_3(target_id, helper_id)
+            .set_contracts(&[&target_instance, &helper_instance])
             .call()
             .await
             .unwrap();
--- tests/src/reentrancy/reentrancy_attacker_contract/src/main.sw
+++ tests/src/reentrancy/reentrancy_attacker_contract/src/main.sw
@@ -1,9 +1,10 @@
 contract;
 
-use std::{auth::*, call_frames::contract_id};
+use std::{auth::*, call_frames::contract_id, constants::ZERO_B256};
 
 use reentrancy_target_abi::Target;
 use reentrancy_attacker_abi::Attacker;
+use reentrancy_attack_helper_abi::AttackHelper;
 
 // Return the sender as a ContractId or panic:
 fn get_msg_sender_id_or_panic() -> ContractId {
@@ -11,6 +12,11 @@ fn get_msg_sender_id_or_panic() -> ContractId {
         Identity::ContractId(v) => v,
         _ => revert(0),
     }
+}
+
+storage {
+    target_id: ContractId = ContractId::from(ZERO_B256),
+    helper: ContractId = ContractId::from(ZERO_B256),
 }
 
 impl Attacker for Contract {
@@ -26,6 +32,13 @@ impl Attacker for Contract {
         abi(Target, target.value).cross_function_reentrance_denied();
     }
 
+    #[storage(write)]
+    fn launch_thwarted_attack_3(target: ContractId, helper: ContractId) {
+        storage.target_id.write(target);
+        storage.helper.write(helper);
+        abi(Target, target.value).cross_contract_reentrancy_denied();
+    }
+
     fn innocent_call(target: ContractId) {
         abi(Target, target.value).guarded_function_is_callable();
     }
@@ -39,7 +52,13 @@ impl Attacker for Contract {
     }
 
     fn evil_callback_3() {
-        abi(Attacker, contract_id().value).launch_thwarted_attack_1(get_msg_sender_id_or_panic());
+        abi(Attacker, contract_id().value).launch_thwarted_attack_2(get_msg_sender_id_or_panic());
+    }
+
+    #[storage(read)]
+    fn evil_callback_4() {
+        let helper = storage.helper.read();
+        abi(AttackHelper, helper.value).attempt_cross_contract_reentrancy(storage.target_id.read());
     }
 
     fn innocent_callback() {}
