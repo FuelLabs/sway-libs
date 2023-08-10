f9f313e91de8d442b928b9c01b298a9ee2efed5f
Sophie <sophiedankel@gmail.com>
2023-08-09 19:31:22 -0700

    Health check API

--- src/main.rs
+++ src/main.rs
@@ -198,12 +198,18 @@ fn not_found() -> String {
     "Not found".to_string()
 }
 
+// Catch 404 not founds.
+#[get("/health")]
+fn health() -> String {
+    "true".to_string()
+}
+
 // Launch the rocket server.
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Cors)
-        .mount("/", routes![compile, all_options])
+        .mount("/", routes![compile, all_options, health])
         .register("/", catchers![not_found])
 }
 
