22f87cc83cd679e99efef1de7efdcf135ac0031c
Sophie <sophiedankel@gmail.com>
2024-02-20 17:02:32 -0800

    revert url

--- app/src/features/editor/hooks/useCompile.tsx
+++ app/src/features/editor/hooks/useCompile.tsx
@@ -51,7 +51,7 @@ export function useCompile(
     setResults([<>Compiling...</>]);
 
     // TODO: Determine the URL based on the NODE_ENV.
-    const server_uri = 'https://prod-2-api.sway-playground.org/compile';
+    const server_uri = 'https://api.sway-playground.org/compile';
     // const server_uri = 'http://0.0.0.0:8080/compile';
     const request = new Request(server_uri, {
       method: 'POST',
