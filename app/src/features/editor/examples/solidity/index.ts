e38eec5d2eb19572d7815798a1a36b55605a27bd
Sophie <sophiedankel@gmail.com>
2024-06-03 11:46:14 -0700

    chore: Add ERC20 solidity example back

--- app/src/features/editor/examples/solidity/index.ts
+++ app/src/features/editor/examples/solidity/index.ts
@@ -1,12 +1,11 @@
 import { ExampleMenuItem } from "../../components/ExampleDropdown";
 import { EXAMPLE_SOLIDITY_CONTRACT_COUNTER } from "./counter";
-// import { EXAMPLE_SOLIDITY_CONTRACT_ERC20 } from "./erc20";
+import { EXAMPLE_SOLIDITY_CONTRACT_ERC20 } from "./erc20";
 import { EXAMPLE_SOLIDITY_CONTRACT_VOTING } from "./voting";
 
 export const EXAMPLE_SOLIDITY_CONTRACTS: ExampleMenuItem[] = [
     { label: 'Counter.sol', code: EXAMPLE_SOLIDITY_CONTRACT_COUNTER },
-    // TODO: add this back once it works again with charcoal.
-    // { label: 'ERC20.sol', code: EXAMPLE_SOLIDITY_CONTRACT_ERC20 },
+    { label: 'ERC20.sol', code: EXAMPLE_SOLIDITY_CONTRACT_ERC20 },
     { label: 'Voting.sol', code: EXAMPLE_SOLIDITY_CONTRACT_VOTING },
   ];
   
