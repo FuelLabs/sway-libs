8fc6e05a3d1b127d1fa03cebbd25d0273a69cb0d
bitzoic <bitzoic.eth@gmail.com>
2024-07-08 18:40:45 +0800

    Update test harness for file name change

--- tests/src/signed_integers/mod.rs
+++ tests/src/signed_integers/mod.rs
@@ -4,8 +4,9 @@ mod signed_i256;
 mod signed_i32;
 mod signed_i64;
 mod signed_i8;
-
-mod signed_i16_twos_complement;
-mod signed_i32_twos_complement;
-mod signed_i64_twos_complement;
-mod signed_i8_twos_complement;
+mod signed_i8_wrapping_neg;
+mod signed_i16_wrapping_neg;
+mod signed_i32_wrapping_neg;
+mod signed_i64_wrapping_neg;
+mod signed_i128_wrapping_neg;
+mod signed_i256_wrapping_neg;
