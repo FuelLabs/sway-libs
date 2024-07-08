377081ed86e9719c0f0df21144887ba95f9b363a
bitzoic <bitzoic.eth@gmail.com>
2024-07-08 18:41:25 +0800

    Run Rust formatter

--- tests/src/signed_integers/mod.rs
+++ tests/src/signed_integers/mod.rs
@@ -1,12 +1,12 @@
 mod signed_i128;
+mod signed_i128_wrapping_neg;
 mod signed_i16;
+mod signed_i16_wrapping_neg;
 mod signed_i256;
+mod signed_i256_wrapping_neg;
 mod signed_i32;
+mod signed_i32_wrapping_neg;
 mod signed_i64;
+mod signed_i64_wrapping_neg;
 mod signed_i8;
 mod signed_i8_wrapping_neg;
-mod signed_i16_wrapping_neg;
-mod signed_i32_wrapping_neg;
-mod signed_i64_wrapping_neg;
-mod signed_i128_wrapping_neg;
-mod signed_i256_wrapping_neg;
